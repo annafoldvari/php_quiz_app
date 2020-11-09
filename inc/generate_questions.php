ac5fd19618eb41470ea7b7e4306c4e85c8b16ed0
anna_f <annafoldvari@gmx.co.uk>
2020-11-09 19:40:06 +0000

    Some styling changes and avoidence of a potential bug.

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -13,10 +13,12 @@ for($i = 0; $i < $number_of_questions; $i++) {
   $inner_array['leftAdder'] = rand(1,100);
   $inner_array['rightAdder'] = rand(1,100);
   $inner_array['correctAnswer'] = $inner_array['leftAdder'] + $inner_array['rightAdder'];
+  do {
   $inner_array['firstIncorrectAnswer'] = $inner_array['correctAnswer'] + rand(-10,10);
+  } while ($inner_array['firstIncorrectAnswer'] == $inner_array['correctAnswe']);
   do {
     $inner_array['secondIncorrectAnswer'] = $inner_array['correctAnswer'] + rand(-10,10);;
-  } while ($inner_array['firstIncorrectAnswer'] == $inner_array['secondIncorrectAnswer']);
+  } while ($inner_array['firstIncorrectAnswer'] == $inner_array['secondIncorrectAnswer'] OR $inner_array['secondIncorrectAnswer'] == $inner_array['correctAnswe']);
   $questions[] = $inner_array;
 }
 
